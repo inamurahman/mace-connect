ea79c889198b7c502e9a02061c5a7b0d519ac5a9
inamurahman <inamvkd@gmail.com>
2023-11-29 22:08:34 +0530

    create event signup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, redirect, url_for, request, session, make_response
 from functools import wraps
-from database import engine, load_user_from_db
+from database import engine, load_user_from_db, get_user_id, get_user, load_faculty_from_db
 from sqlalchemy import text
 import os
 app = Flask(__name__)
@@ -8,8 +8,6 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 
 events = [{'id': 1, 'name': 'takshak', 'date': '2019-01-01', 'description':"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", 'venue':'Seminar hall 2'}, {'id': 2, 'name': 'sanskriti', 'date': '2019-01-01', 'description':"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", 'venue':'ootpra'}, {'id': 3, 'name': 'dj night', 'date': '2019-01-01', 'description':"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", 'venue': 'basket ball court'}, {'id': 4, 'name': 'dj night', 'date': '2019-01-01', 'description':"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", 'venue': 'basket ball court'}, {'id': 5, 'name': 'dj night', 'date': '2019-01-01', 'description':"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", 'venue': 'basket ball court'}, {'id': 6, 'name': 'dj night', 'date': '2019-01-01', 'description':"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", 'venue': 'basket ball court'}]
-
-users = load_user_from_db()
 
 # users = {'trial@gmail.com': {'password': 'secret', 'type':'admin'}}
 
@@ -48,8 +46,12 @@ def login():
     username = request.form['username']
     password = request.form['password']
     remember_me = 'remember_me' in request.form
-    if username in users and users[username]['password'] == password:
-        session['username'] = username
+
+    user = get_user(username)
+
+    if user['password'] == password:
+        session['username'] = user['username']
+        session['usertype'] = user['usertype']
         if remember_me:
             session.permanent = True
             response = make_response(redirect(url_for('home')))
@@ -64,12 +66,96 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('username', None)
+    session.pop('usertype', None)
     return render_template('login.html', error="You have been logged out")
 
 @app.route('/api/events')
 def api_events():
     return jsonify(events)
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
     
+    
+
+@app.route('/signup/<string:usertype>', methods=['GET', 'POST'])
+def signup_form(usertype):
+    types = ['student', 'faculty', 'organizer']
+    if usertype not in types:
+        return "Invalid user type", 404
+    if request.method == 'GET':
+        classes = ['S1DS','S1CS','S1ME','S2DS','S2CS','S2ME','S3DS','S3CS','S3ME','S4DS','S4CS','S4ME','S5DS','S5CS','S5ME','S6DS','S6CS','S6ME','S7DS','S7CS','S7ME','S8DS','S8CS','S8ME']
+        departments = ['Computer Science', 'Mechanical', 'Electrical', 'Electronics', 'Civil']
+        return render_template('signupForm.html', usertype=usertype,  classlist=classes, deptlist=departments)
+    
+    users = load_user_from_db()
+    if usertype == 'student':
+        print(request.form)
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        fname = request.form['firstname']
+        lname = request.form['lastname']
+        batch = request.form['class']
+        mobile = request.form['mobile']
+        if username in users:
+            return render_template('signupForm.html', usertype=usertype, error="Username already exists")
+        with engine.connect() as connection:
+            stmt = text("INSERT INTO Users (Username, Password, UserType, Email) VALUES (:user, :passwd, :type, :mail )").bindparams(user=username, passwd=password, type=usertype, mail=email)
+            result = connection.execute(stmt)
+            stmt = text("INSERT INTO UserDetails (FirstName, LastName, Class, Mobile, UserID) VALUES (:fname, :lname, :batch, :mobile, :id)").bindparams(fname=fname, lname=lname, batch=batch, mobile=mobile, id=get_user_id(username))
+            result = connection.execute(stmt)
+        return redirect(url_for('login'))
+    
+    elif usertype == 'faculty':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        fname = request.form['firstname']
+        lname = request.form['lastname']
+        mobile = request.form['mobile']
+        dept = request.form['dept']
+        if username in users:
+            return render_template('signupForm.html', usertype=usertype, error="Username already exists")
+        
+        with engine.connect() as connection:
+            stmt = text("INSERT INTO Users (Username, Password, UserType, Email) VALUES (:user, :passwd, :type, :mail )").bindparams(user=username, passwd=password, type=usertype, mail=email)
+            result = connection.execute(stmt)
+            stmt = text("INSERT INTO UserDetails (FirstName, LastName, Department, Mobile, UserID) VALUES (:fname, :lname, :dept, :mobile, :id)").bindparams(fname=fname, lname=lname, dept=dept, mobile=mobile, id=get_user_id(username))
+            result = connection.execute(stmt)
+        return redirect(url_for('login'))
+    
+    elif usertype == 'organizer':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        orgname = request.form['orgname']
+        orgdesc = request.form['orgdesc']
+        facultyadv = request.form['faculty']
+        
+        with engine.connect() as connection:
+            stmt = text("INSERT INTO Users (Username, Password, UserType, Email) VALUES (:user, :passwd, :type, :mail )").bindparams(user=username, passwd=password, type=usertype, mail=email)
+            result = connection.execute(stmt)
+            stmt = text("INSERT INTO Organizer (OrganizerName, Description, AdvisorUsername, UserID) VALUES (:orgname, :orgdesc, :facultyadv)").bindparams(orgname=orgname, orgdesc=orgdesc, facultyadv=facultyadv)
+        return redirect(url_for('login'))
+
+@app.route('/createevent', methods=['GET', 'POST'])
+def create_event():
+    if request.method == 'GET':
+        return render_template('createevent.html', faculties=load_faculty_from_db(),categorylist=['Technical', 'Cultural', 'Sports', 'Workshop', 'Seminar', 'Other'])
+    
+    name = request.form['name']
+    date = request.form['date']
+    description = request.form['description']
+    venue = request.form['venue']
+    category = request.form['category']
+    faculty = request.form['faculty']
+
+    with engine.connect() as connection:
+        smtp = text("INSERT INTO Events (EventName, Date, Description, Location, Category, OrganizerID, RequestedBy) VALUES (:name, :date, :description, :venue, :category, :organizerid)").bindparams(name=name, date=date, description=description, venue=venue, category=category, organizerid=get_user_id(session['username'], faculty=faculty))
+        result = connection.execute(smtp)
+    return redirect(url_for('home'))
+
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -3,7 +3,6 @@ import os
 
 ssl_args = {'ssl': {'ca': '/etc/ssl/certs/ca-certificates.crt'}}
 db_string = os.environ.get('DB_CONNECTION_MACEDB')
-print(db_string)
 engine = create_engine(db_string, connect_args=ssl_args)
 
 
@@ -12,9 +11,31 @@ def load_user_from_db():
         result = connection.execute(text("SELECT UserID,Username,Password,UserType FROM Users"))
         users = result.all()
 
-        users = {user[1]: {'password': user[2], 'usertype': user[3]} for user in users}
+        users = {user[1]: {'password': user[2], 'usertype': user[3],'userid': user[0]} for user in users}
 
     return users
 
-k = load_user_from_db()
-print(k)
+def get_user_id(username):
+    with engine.connect() as connection:
+        stmt = text("SELECT UserID FROM Users WHERE Username=:username").bindparams(username=username)
+        result = connection.execute(stmt)
+        user_id = result.first()[0]
+    return user_id
+
+def get_user(username):
+    with engine.connect() as connection:
+        stmt = text("SELECT UserID,Username,Password,UserType FROM Users WHERE Username=:username").bindparams(username=username)
+        result = connection.execute(stmt)
+        user = result.first()
+        if user is None:
+            return None
+        user = {'userid': user[0], 'username': user[1], 'password': user[2], 'usertype': user[3]}
+    return user 
+
+def load_faculty_from_db():
+    with engine.connect() as connection:
+        smtp = text("SELECT Users.username, UserDetails.UserID, UserDetails.FirstName, UserDetails.LastName, UserDetails.Mobile, UserDetails.Department FROM UserDetails JOIN Users ON UserDetails.UserID = Users.UserID WHERE UserDetails.UserID IN (SELECT UserID FROM Users WHERE UserType='faculty');")
+        result = connection.execute(smtp)
+        faculty = result.all()
+        faculty = [{'username': user[0], 'userid': user[1], 'firstname': user[2], 'lastname': user[3], 'mobile': user[4], 'department': user[5]} for user in faculty]
+    return faculty
